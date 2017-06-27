779ad4d2b00ba287ecc2ff8994ebf815b7990c0a
wanggeng <mtwanggeng@damai.cn>
2017-06-27 13:27:30 +0800

    code review

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 
 > A Vue.js project
 
+# 创建项目的指令
+```
+vue init webpack learn-front
+```
+
 ## Build Setup
 
 ``` bash
 # install dependencies
 npm install
+
+此处如果安装了yarn也可以直接使用yarn安装
+yarn
 
 # serve with hot reload at localhost:8080
 npm run dev
@@ -19,3 +27,7 @@ npm run build --report
 ```
 
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+# 项目的目录结构
+.eslintrc.js
+修改默认的semi结尾的问题
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+import CSSLayout from '@/components/CSSLayout';
+import Home from '@/components/Home';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
+      path: '/css-layout',
+      name: 'CSSLayout',
+      component: CSSLayout
+    },
+    {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
     }
   ]
-})
+});
